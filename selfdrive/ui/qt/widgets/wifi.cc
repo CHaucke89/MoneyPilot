8b7d057aec24d0db9c2905dd9a6f1cba067c7c1c
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2025-08-28 21:54:19 -0400

    Home UI
    Missing this guy
    
    Genius

--- selfdrive/ui/qt/widgets/wifi.cc
+++ selfdrive/ui/qt/widgets/wifi.cc
@@ -6,32 +6,32 @@
 #include <QPushButton>
 
 WiFiPromptWidget::WiFiPromptWidget(QWidget *parent) : QFrame(parent) {
-  // Setup Firehose Mode
+
   QVBoxLayout *main_layout = new QVBoxLayout(this);
   main_layout->setContentsMargins(56, 40, 56, 40);
-  main_layout->setSpacing(42);  
-  
-  QLabel *title = new QLabel(tr("<span style='font-family: \"Noto Color Emoji\";'>🔥</span> Firehose Mode <span style='font-family: Noto Color Emoji;'>🔥</span>"));
+  main_layout->setSpacing(42);
+
+  QLabel *title = new QLabel(tr("Model Selector"));
   title->setStyleSheet("font-size: 64px; font-weight: 500;");
   main_layout->addWidget(title);
 
-  QLabel *desc = new QLabel(tr("Maximize your training data uploads to improve openpilot's driving models."));
+  QLabel *desc = new QLabel(tr("Press to go to the model selector."));
   desc->setStyleSheet("font-size: 40px; font-weight: 400;");
   desc->setWordWrap(true);
   main_layout->addWidget(desc);
 
-  QPushButton *settings_btn = new QPushButton(tr("Open"));
-  connect(settings_btn, &QPushButton::clicked, [=]() { emit openSettings(1, "FirehosePanel"); });
+  QPushButton *settings_btn = new QPushButton(tr("Models"));
+  connect(settings_btn, &QPushButton::clicked, [=]() { emit openSettings(5, "ModelsPanel"); });
   settings_btn->setStyleSheet(R"(
     QPushButton {
       font-size: 48px;
       font-weight: 500;
       border-radius: 10px;
-      background-color: #465BEA;
+      background-color: #F44336;
       padding: 32px;
     }
     QPushButton:pressed {
-      background-color: #3049F4;
+      background-color: #D83E33;
     }
   )");
   main_layout->addWidget(settings_btn);
