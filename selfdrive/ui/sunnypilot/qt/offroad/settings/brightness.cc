bbeedd2766a765f5ce27fd78c4734ebf56bc5d9c
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2025-10-09 10:53:40 -0400

    Revert "UI: Move Brightness & Interactivity Timeout to Display Panel (#1326)"
    This reverts commit 517020ffb6b6f9443360e9bb55c4dcc680e22e32.

--- selfdrive/ui/sunnypilot/qt/offroad/settings/brightness.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/brightness.cc
@@ -25,8 +25,8 @@ const QMap<QString, QString> Brightness::brightness_options = {
 
 Brightness::Brightness() : OptionControlSP(
   "Brightness",
-  tr("Global Brightness"),
-  tr("Overrides the brightness of the device. This applies to both onroad and offroad screens. "),
+  tr("Brightness"),
+  tr("Overrides the brightness of the device."),
   "../assets/offroad/icon_blank.png",
   {0, 11}, 1, true, &brightness_options) {
 
