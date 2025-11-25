e5e550625ac2a831edd869a091a8902e63c30181
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2025-11-25 15:34:27 -0500

    Always off

--- common/params_keys.h
+++ common/params_keys.h
@@ -262,6 +262,8 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"TorqueParamsOverrideEnabled", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"TorqueParamsOverrideFriction", {PERSISTENT | BACKUP, FLOAT, "0.1"}},
     {"TorqueParamsOverrideLatAccelFactor", {PERSISTENT | BACKUP, FLOAT, "2.5"}},
+
+    {"DisableDM", {PERSISTENT | BACKUP, BOOL, "1"}},
     {"DoSoftReboot", {CLEAR_ON_MANAGER_START, BOOL}},
     {"DynamicDamp", {PERSISTENT | BACKUP, BOOL, "1"}},
     {"DynamicDeltas", {PERSISTENT | BACKUP, BOOL, "1"}},
