02c0f490038af8eebf00de78e29c07761ab0742c
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-04-12 13:50:35 -0400

    Plan plus controls

--- selfdrive/ui/sunnypilot/layouts/settings/models.py
+++ selfdrive/ui/sunnypilot/layouts/settings/models.py
@@ -43,7 +43,7 @@ class ModelsLayout(Widget):
     self._initialize_items()
 
     self.clear_cache_item.action_item.set_value(f"{self.calculate_cache_size():.2f} MB")
-    for ctrl, key in [(self.camera_offset, "CameraOffset"), (self.lane_turn_value_control, "LaneTurnValue"), (self.delay_control, "LagdToggleDelay")]:
+    for ctrl, key in [(self.camera_offset, "CameraOffset"), (self.plan_plus, "PlanplusControl"), (self.lane_turn_value_control, "LaneTurnValue"), (self.delay_control, "LagdToggleDelay")]:
       ctrl.action_item.set_value(int(float(ui_state.params.get(key, return_default=True)) * 100))
 
     self._scroller = Scroller(self.items, line_separator=True, spacing=0)
@@ -80,6 +80,11 @@ class ModelsLayout(Widget):
                                                   1, None, True, "", style.BUTTON_ACTION_WIDTH, None, True,
                                                   lambda v: f"{v / 100:.2f}m")
 
+    self.plan_plus = option_item_sp(tr("Plan Plus Control"), "PlanplusControl", 0, 200,
+                                                  tr("Adjust model recentering strength."),
+                                                  1, None, True, "", style.BUTTON_ACTION_WIDTH, None, True,
+                                                  lambda v: f"{v / 100:.2f}")
+
     self.lane_turn_value_control = option_item_sp(tr("Adjust Lane Turn Speed"), "LaneTurnValue", 500, 2000,
                                                   tr("Set the maximum speed for lane turn desires. Default is 19 mph."),
                                                   int(round(100 / CV.MPH_TO_KPH)), None, True, "", style.BUTTON_ACTION_WIDTH, None, True,
@@ -98,8 +103,8 @@ class ModelsLayout(Widget):
 
     self.lagd_toggle = toggle_item_sp(tr("Live Learning Steer Delay"), "", param="LagdToggle")
 
-    self.items = [self.current_model_item, self.cancel_download_item, self.supercombo_label, self.vision_label,
-                  self.policy_label, self.off_policy_label, self.on_policy_label, self.refresh_item, self.clear_cache_item, self.camera_offset,
+    self.items = [self.current_model_item, self.cancel_download_item, self.supercombo_label, self.vision_label, self.policy_label, self.off_policy_label,
+                  self.on_policy_label, self.refresh_item, self.clear_cache_item, self.camera_offset, self.plan_plus,
                   self.lane_turn_desire_toggle, self.lane_turn_value_control, self.lagd_toggle, self.delay_control]
 
   def _update_lagd_description(self, lagd_toggle: bool):
