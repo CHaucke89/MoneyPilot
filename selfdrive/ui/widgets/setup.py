7be90e00a533e02db5390b20dda67aa54b842e61
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-04-17 17:10:51 -0400

    Forgot to use the enter offroad button

--- selfdrive/ui/widgets/setup.py
+++ selfdrive/ui/widgets/setup.py
@@ -20,9 +20,9 @@ class SetupWidget(Widget):
                                      button_style=ButtonStyle.PRIMARY)
     self._firehose_label = Label(lambda: tr("🔥 Firehose Mode 🔥"), font_weight=FontWeight.MEDIUM, font_size=64)
     self._offroad_label = Label(lambda: tr("Always Offroad Mode"), font_weight=FontWeight.MEDIUM, font_size=64)
-    self._enter_offroad_btn = Button(lambda: tr("Enter"), lambda: ui_state.params.put_bool("OffroadMode", True),
+    self._enter_offroad_btn = Button(lambda: tr("Enter"), lambda: ui_state.params.put_bool_nonblocking("OffroadMode", True),
                                      button_style=ButtonStyle.PRIMARY)
-    self._exit_offroad_btn = Button(lambda: tr("Exit"), lambda: ui_state.params.put_bool("OffroadMode", False),
+    self._exit_offroad_btn = Button(lambda: tr("Exit"), lambda: ui_state.params.put_bool_nonblocking("OffroadMode", False),
                                      button_style=ButtonStyle.DANGER)
 
   def set_open_settings_callback(self, callback):
@@ -116,7 +116,11 @@ class SetupWidget(Widget):
     # Exit
     button_height = 48 + 64  # font size + padding
     button_rect = rl.Rectangle(x, y, w, button_height)
-    self._exit_offroad_btn.render(button_rect)
+    if ui_state.params.get_bool("OffroadMode"):
+      self._exit_offroad_btn.render(button_rect)
+    else:
+      self._enter_offroad_btn.render(button_rect)
+
 
   def __del__(self):
     if self._pairing_dialog:
