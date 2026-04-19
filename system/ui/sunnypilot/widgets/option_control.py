bc054622487a1cf57c827e11bcb7ce728ecbf564
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-04-19 07:03:48 -0400

    Modify option control to reset to default when value is tapped

--- system/ui/sunnypilot/widgets/option_control.py
+++ system/ui/sunnypilot/widgets/option_control.py
@@ -53,6 +53,7 @@ class OptionControlSP(ItemAction):
     # Layout rectangles for components
     self.minus_btn_rect = rl.Rectangle(0, 0, 0, 0)
     self.plus_btn_rect = rl.Rectangle(0, 0, 0, 0)
+    self.label_rect = rl.Rectangle(0, 0, 0, 0)
 
   def get_value(self) -> int:
     """Get the current value of the control"""
@@ -157,6 +158,18 @@ class OptionControlSP(ItemAction):
 
     rl.draw_text_ex(self._font, text, rl.Vector2(text_x, text_y), VALUE_FONT_SIZE, 0, text_color)
 
+  def reset_to_default(self):
+    default = self.params.get_default_value(self.param_key)
+    if default is None:
+      return
+    if self.value_map:
+      for k, v in self.value_map.items():
+        if v == default:
+          self.set_value(int(k))
+          return
+    else:
+      self.set_value(int(float(default) * 100.0) if self.use_float_scaling else int(default))
+
   def _handle_mouse_release(self, mouse_pos: MousePos):
     if self._minus_enabled and rl.check_collision_point_rec(mouse_pos, self.minus_btn_rect):
       new_value = self.current_value - self.value_change_step
@@ -166,3 +179,5 @@ class OptionControlSP(ItemAction):
       new_value = self.current_value + self.value_change_step
       new_value = min(self.max_value, new_value)
       self.set_value(new_value)
+    elif self.enabled and rl.check_collision_point_rec(mouse_pos, self.label_rect):
+      self.reset_to_default()
