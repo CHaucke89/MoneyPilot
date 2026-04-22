5eecc2d8c496e4009542c4d1da25d92b1c5ae1aa
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-04-22 17:10:31 -0400

    Version

--- release/ci/publish.sh
+++ release/ci/publish.sh
@@ -30,7 +30,7 @@ if [ -z "$GIT_ORIGIN" ]; then
 fi
 
 # "Tagging"
-echo "#define SUNNYPILOT_VERSION \"$VERSION\"" > ${OUTPUT_DIR}/sunnypilot/common/version.h
+echo "#define CLOUDYPILOT_VERSION \"$VERSION\"" > ${OUTPUT_DIR}/sunnypilot/common/version.h
 
 ## set git identity
 #source $DIR/identity.sh
@@ -47,7 +47,7 @@ git rm -rf $OUTPUT_DIR/.git || true # Doing cleanup, but it might fail if the .g
 git remote remove origin || true # ensure cleanup
 git remote add origin $GIT_ORIGIN
 #git push origin -d $DEV_BRANCH || true # Ensuring we delete the remote branch if it exists as we are wiping it out
-git fetch origin $DEV_BRANCH || (git checkout -b $DEV_BRANCH && git commit --allow-empty -m "sunnypilot v$VERSION release" && git push -u origin $DEV_BRANCH)
+git fetch origin $DEV_BRANCH || (git checkout -b $DEV_BRANCH && git commit --allow-empty -m "cloudypilot v$VERSION release" && git push -u origin $DEV_BRANCH)
 
 echo "[-] committing version $VERSION T=$SECONDS"
 git add -f .
@@ -55,11 +55,11 @@ git add -f .
 # include source commit hash and build date in commit
 GIT_HASH=$(git --git-dir=$SOURCE_DIR/.git rev-parse HEAD)
 DATETIME=$(date '+%Y-%m-%dT%H:%M:%S')
-SP_VERSION=$(awk -F\" '{print $2}' $SOURCE_DIR/sunnypilot/common/version.h)
+CP_VERSION=$(awk -F\" '{print $2}' $SOURCE_DIR/sunnypilot/common/version.h)
 
 # Commit with detailed message
-git commit -a -m "sunnypilot v$VERSION
-version: sunnypilot v$SP_VERSION (${EXTRA_VERSION_IDENTIFIER})
+git commit -a -m "cloudypilot v$VERSION
+version: cloudypilot v$CP_VERSION (${EXTRA_VERSION_IDENTIFIER})
 date: $DATETIME
 master commit: $GIT_HASH
 "
@@ -68,4 +68,5 @@ git branch -m $DEV_BRANCH
 
 # Push!
 echo "[-] pushing T=$SECONDS"
+git lfs uninstall
 git push -f origin $DEV_BRANCH
