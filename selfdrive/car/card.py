65d6416dd76627a4b9a79a77888428aca9ffedd7
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-05-25 12:42:58 -0400

    Custom button controls live tuning toggle instead

--- selfdrive/car/card.py
+++ selfdrive/car/card.py
@@ -200,7 +200,7 @@ class Car:
 
     for e in CS.buttonEvents:
       if e.type == car.CarState.ButtonEvent.Type.altButton2 and e.pressed:
-        self.params.put_bool_nonblocking("OffroadMode", not self.params.get_bool("OffroadMode"))
+        self.params.put_bool_nonblocking("TorqueParamsOverrideEnabled", not self.params.get_bool("TorqueParamsOverrideEnabled"))
 
     # Update radar tracks from CAN
     RD: structs.RadarDataT | None = self.RI.update(can_list)
