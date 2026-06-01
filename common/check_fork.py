b0dd97cba35e6855e273b0a34313a7b2a401f11b
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-06-01 10:48:40 -0400

    common: fix cloudypilot fork check path

--- common/check_fork.py
+++ common/check_fork.py
@@ -1,7 +1,7 @@
 import os
 
 def _is_cloudypilot():
-    version_file = "sunnypilot/common/version.h"
+    version_file = "cloudypilot/common/version.h"
     if os.path.exists(version_file):
         with open(version_file) as f:
             if "CLOUDYPILOT_VERSION" in f.read():
