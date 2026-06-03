6d8c338e9ce9adb22ee704c4b76a9e47c63efe34
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-06-02 21:24:29 -0400

    ui: conditionally load cloudypilot border colors

--- selfdrive/ui/onroad/augmented_road_view.py
+++ selfdrive/ui/onroad/augmented_road_view.py
@@ -34,8 +34,10 @@ BORDER_COLORS = {
   UIStatus.OVERRIDE: rl.Color(0x89, 0x92, 0x8D, 0xFF),  # Gray for override state
   UIStatus.ENGAGED: rl.Color(0x16, 0x7F, 0x40, 0xFF),  # Green for engaged state
   **BORDER_COLORS_SP,
-  **BORDER_COLORS_CP,
 }
+
+if gui_app.cloudypilot_ui():
+  BORDER_COLORS |= {**BORDER_COLORS_CP}
 
 WIDE_CAM_MAX_SPEED = 10.0  # m/s (22 mph)
 ROAD_CAM_MIN_SPEED = 15.0  # m/s (34 mph)
