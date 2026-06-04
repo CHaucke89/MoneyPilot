c311fdd489c719b7215788d510fea488bddf6208
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-06-04 10:24:48 -0400

    Move reset functionality to option_control_cp

---
+++ system/ui/cloudypilot/widgets/option_control.py
@@ -0,0 +1,30 @@
+import pyray as rl
+from collections.abc import Callable
+from openpilot.system.ui.sunnypilot.widgets.option_control import OptionControlSP, LABEL_WIDTH
+from openpilot.system.ui.lib.application import MousePos
+
+
+
+class OptionControlCP(OptionControlSP):
+  def __init__(self, param: str, min_value: int, max_value: int,
+               value_change_step: int = 1, enabled: bool | Callable[[], bool] = True,
+               on_value_changed: Callable[[int], None] | None = None,
+               value_map: dict[int, int] | None = None,
+               label_width: int = LABEL_WIDTH,
+               use_float_scaling: bool = False, label_callback: Callable[[int], str] | None = None,
+               reset_enabled: bool = False):
+
+    super().__init__()
+    self._reset_enabled = reset_enabled
+
+  def _handle_mouse_release(self, mouse_pos: MousePos):
+    if self._minus_enabled and rl.check_collision_point_rec(mouse_pos, self.minus_btn_rect):
+      new_value = self.current_value - self.value_change_step
+      new_value = max(self.min_value, new_value)
+      self.set_value(new_value)
+    elif self._plus_enabled and rl.check_collision_point_rec(mouse_pos, self.plus_btn_rect):
+      new_value = self.current_value + self.value_change_step
+      new_value = min(self.max_value, new_value)
+      self.set_value(new_value)
+    elif self._reset_enabled and rl.check_collision_point_rec(mouse_pos, self.label_rect):
+      self.reset_to_default()
