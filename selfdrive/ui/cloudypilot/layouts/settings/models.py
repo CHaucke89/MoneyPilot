9bf7da597cab11d8fd15d4a8f0a44f45ebb1ade6
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-06-04 10:53:57 -0400

    ui: set camera offset from params
    option_item_cp

--- selfdrive/ui/cloudypilot/layouts/settings/models.py
+++ selfdrive/ui/cloudypilot/layouts/settings/models.py
@@ -8,7 +8,7 @@ from openpilot.selfdrive.ui.ui_state import device, ui_state
 from openpilot.system.ui.cloudypilot.lib.styles import style
 from openpilot.system.ui.cloudypilot.widgets.toggle import ON_COLOR
 from openpilot.selfdrive.ui.sunnypilot.layouts.settings.models import ModelsLayout
-from openpilot.system.ui.cloudypilot.widgets.list_view import option_item_cp as option_item_sp
+from openpilot.system.ui.cloudypilot.widgets.list_view import option_item_cp
 from openpilot.system.ui.sunnypilot.widgets.list_view import LineSeparatorSP
 
 
@@ -16,10 +16,13 @@ class ModelsLayoutCP(ModelsLayout):
   def _initialize_items(self):
     super()._initialize_items()
 
-    self.camera_offset = option_item_sp(tr("Camera Offset"), "CameraOffset", -35, 35,
+    self.camera_offset = option_item_cp(tr("Camera Offset"), "CameraOffset", -35, 35,
                                                   tr("Adjust camera offset to keep vehicle centered."),
                                                   1, None, True, "", style.BUTTON_ACTION_WIDTH, None, True,
                                                   lambda v: f"{v / 100:.2f}m", True, True)
+
+    for ctrl, key in [(self.camera_offset, "CameraOffset"),]:
+      ctrl.action_item.set_value(int(float(ui_state.params.get(key, return_default=True)) * 100))
 
     camera_offset_index = next(i for i, item in enumerate(self.items) if item is self.clear_cache_item)
     insert_pos = camera_offset_index + 1
