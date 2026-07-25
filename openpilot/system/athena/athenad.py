6767cc8ec369bb3023efb3989d70f25a00724537
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-07-25 05:20:56 -0400

    support alternative Konik API

--- openpilot/system/athena/athenad.py
+++ openpilot/system/athena/athenad.py
@@ -50,7 +50,7 @@ from openpilot.common.hardware.hw import Paths
 from openpilot.system.athena.rpc import dispatcher, handle, is_call, is_response, loads
 
 
-ATHENA_HOST = os.getenv('ATHENA_HOST', 'wss://athena.comma.ai')
+ATHENA_HOST = os.getenv('ATHENA_HOST', 'wss://athena.konik.ai' if Params().get_bool("KonikApi") else 'wss://athena.comma.ai')
 HANDLER_THREADS = int(os.getenv('HANDLER_THREADS', "4"))
 LOCAL_PORT_WHITELIST = {22, }  # SSH
 
