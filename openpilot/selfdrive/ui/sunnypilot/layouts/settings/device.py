347f415e8cd81ae27c4571eab1c78487ca5ddbeb
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-07-31 09:35:08 -0400

    go away

--- openpilot/selfdrive/ui/sunnypilot/layouts/settings/device.py
+++ openpilot/selfdrive/ui/sunnypilot/layouts/settings/device.py
@@ -150,14 +150,6 @@ class DeviceLayoutSP(DeviceLayout):
     self._reboot_btn = self._power_buttons.action_item.left_button
     self._power_btn = self._power_buttons.action_item.right_button
 
-    items += [
-      dual_button_item_sp(
-        left_text=lambda: tr("Reboot"),
-        right_text=lambda: tr("Power Off"),
-        left_callback=self._reboot_prompt,
-        right_callback=self._power_off_prompt),
-    ]
-
     return items
 
   def _offroad_transition(self):
