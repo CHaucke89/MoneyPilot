9a73ae25bee5ec927e0f65413820fc8ee1af26bc
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-08-09 08:55:39 -0400

    Refactor home

--- openpilot/selfdrive/ui/layouts/main.py
+++ openpilot/selfdrive/ui/layouts/main.py
@@ -15,6 +15,10 @@ if gui_app.sunnypilot_ui():
   from openpilot.selfdrive.ui.sunnypilot.layouts.settings.settings import SettingsLayoutSP as SettingsLayout
   from openpilot.selfdrive.ui.sunnypilot.layouts.home import HomeLayoutSP as HomeLayout
 
+if gui_app.cloudypilot_ui():
+  from openpilot.selfdrive.ui.cloudypilot.layouts.home import HomeLayoutCP as HomeLayout
+
+
 class MainState(IntEnum):
   HOME = 0
   SETTINGS = 1
