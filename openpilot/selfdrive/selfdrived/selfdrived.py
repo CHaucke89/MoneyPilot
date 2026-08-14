36b016b8d6c16596cd035a5158fdf4120fbe00d5
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-08-14 13:11:28 -0400

    Fix param

--- openpilot/selfdrive/selfdrived/selfdrived.py
+++ openpilot/selfdrive/selfdrived/selfdrived.py
@@ -98,7 +98,7 @@ class SelfdriveD(CruiseHelper):
     # TODO: de-couple selfdrived with card/conflate on carState without introducing controls mismatches
     self.car_state_sock = messaging.sub_sock('carState', timeout=20)
 
-    disable_dm = self.params.get_bool("AlwaysOffDm")
+    disable_dm = self.params.get_bool("AlwaysOffDM")
     ignore = self.sensor_packets + self.gps_packets + ['alertDebug', 'lateralManeuverPlan'] + ['modelDataV2SP', 'longitudinalPlanSP']
     if SIMULATION:
       ignore += ['cabinCameraState', 'managerState']
