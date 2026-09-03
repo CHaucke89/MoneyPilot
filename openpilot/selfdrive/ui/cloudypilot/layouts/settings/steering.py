5eeead7a489b4cbe51087931f2a5cc76d93ab627
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-09-03 05:38:18 -0400

    No need to check our fork here

--- openpilot/selfdrive/ui/cloudypilot/layouts/settings/steering.py
+++ openpilot/selfdrive/ui/cloudypilot/layouts/settings/steering.py
@@ -1,12 +1,9 @@
 
-from openpilot.selfdrive.ui.ui_state import ui_state
 from openpilot.system.ui.lib.multilang import tr
-from openpilot.system.ui.lib.application import gui_app
-from openpilot.system.ui.sunnypilot.widgets.list_view import toggle_item_sp, LineSeparatorSP
+from openpilot.system.ui.sunnypilot.widgets.list_view import toggle_item_sp
 from openpilot.selfdrive.ui.sunnypilot.layouts.settings.steering import SteeringLayout
-if gui_app.cloudypilot_ui():
-  from openpilot.system.ui.cloudypilot.widgets.list_view import LineSeparatorCP as LineSeparatorSP
-  from openpilot.system.ui.cloudypilot.widgets.list_view import option_item_cp
+from openpilot.system.ui.cloudypilot.widgets.list_view import LineSeparatorCP
+from openpilot.system.ui.cloudypilot.widgets.list_view import option_item_cp
 
 
 
@@ -38,7 +35,7 @@ class SteeringLayoutCP(SteeringLayout):
     items += [
       self._sr_toggle,
       self._custom_sr,
-      LineSeparatorSP(40),
+      LineSeparatorCP(40),
     ]
     return items
 
